65037cb139bb595068c887243eadbdee419a83a9
Angat13 <angatw123@gmail.com>
2025-04-22 14:24:58 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://Angat:AngatSingh1313@cluster1313.ecpdi.mongodb.net/CARECRAFT').then(()=>console.log("DB Connected"));
+    await mongoose.connect('YOUR MONGODB KEY').then(()=>console.log("DB Connected"));
    
 }
 
